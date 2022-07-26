ddb7bb00b8e4a04fd885e5ad3c93e8137cc905a9
MyP-VII <mykola.perekytnyi@gmail.com>
2022-07-26 16:00:31 +0200

    home1

---
+++ home1-2-2.py
@@ -0,0 +1,7 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+x = int(input("Введите число x: "))
+if a<x<b:
+    print(True)
+else:
+    print(False)
---
+++ home1-2-3.py
@@ -0,0 +1,8 @@
+a, b, c = int(input("a = ")), int(input("b = ")), int(input("c = "))
+x1 = (-b + ((b ** 2) - 4 * a * c) ** 0.5) / (2 * a)
+x2 = (-b - ((b ** 2) - 4 * a * c) ** 0.5) / (2 * a)
+print("x1 =", x1)
+print("x2 =", x2)
+input()
+
+
